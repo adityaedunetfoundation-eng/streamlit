27bb594da15cb802e4bc95aaf14d3c49522b3146
Aditya <adityaedunetfoundation@gmail.com>
2026-05-18 00:02:05 +0530

    first commit

---
+++ flask.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model
+model = pickle.load(open("iris_model.pkl", "rb"))
+
+# Home page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Prediction
+@app.route("/predict", methods=["POST"])
+def predict():
+    features = [
+        float(request.form["sepal_length"]),
+        float(request.form["sepal_width"]),
+        float(request.form["petal_length"]),
+        float(request.form["petal_width"])
+    ]
+
+    prediction = model.predict([features])
+
+    species = ["Setosa", "Versicolor", "Virginica"]
+
+    result = species[prediction[0]]
+
+    return render_template("index.html", prediction=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
